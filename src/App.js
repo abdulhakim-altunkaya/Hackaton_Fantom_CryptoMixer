40a9305bc94742e3fe2bd07b31846c449f157ced
abdulhakim_altunkaya <altunkaya2@gmail.com>
2023-05-21 21:54:12 +0300

    all test deployment and function checks complete, contract frontend integration complete. now ready for address generation functionality of metamask

--- src/App.js
+++ src/App.js
@@ -3,12 +3,32 @@ import { useAccount } from "./Store.js";
 
 function App() {
   const {ethereum} = window;
+
+  let[account, setAccount] = useState("");
+  const connectMetamask = async () => {
+    if(window.ethereum !== "undefined") {
+      const accounts = await ethereum.request({ method: "eth_requestAccounts"});
+      setAccount(accounts[0]);
+    } else {
+      alert("download metamask extension on your browser")
+    }
+  }
+
+
+  const myContract = useAccount(state => state.contract);
+  let[city, setCity] = useState("");
+  const getData = async () => {
+    let data = await myContract.myNumber();
+    let data2 = data.toString();
+    setCity(data2);
+  }
+
   return (
     <div className="App">
-        <button>CONNECT TO METAMASK</button>
-        <p></p>
-        <button>CONNECT TO CONTRACT</button>
-        <p></p>
+        <button onClick={connectMetamask}>CONNECT TO METAMASK</button>
+        <p>{account}</p>
+        <button onClick={getData}>GET DATA FROM CONTRACT</button>
+        <p>{city}</p>
         <button>TEST BUTTON</button>
         <p></p>
     </div>
