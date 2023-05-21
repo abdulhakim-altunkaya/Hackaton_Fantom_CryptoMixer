52f8a8ac5fa1171560fc1849b6861c5165256b4c
abdulhakim_altunkaya <altunkaya2@gmail.com>
2023-05-21 15:07:35 +0300

    test contract created, css file created, app.js first part created for test deployment and testing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-
+import { useState } from "react";
+import { useAccount } from "./Store.js";
 
 function App() {
+  const {ethereum} = window;
   return (
     <div className="App">
-      <p>hellow man</p>
+        <button>CONNECT TO METAMASK</button>
+        <p></p>
+        <button>CONNECT TO CONTRACT</button>
+        <p></p>
+        <button>TEST BUTTON</button>
+        <p></p>
     </div>
   );
 }
