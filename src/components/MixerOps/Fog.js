43a9792894748b9a3124be486c8fb9e105960bff
abdulhakim_altunkaya <altunkaya2@gmail.com>
2023-06-23 12:09:38 +0300

    code refactoring, allowance component is removed and integrated inside getBalances component

--- src/components/MixerOps/Fog.js
+++ src/components/MixerOps/Fog.js
@@ -6,14 +6,12 @@ import FogDeposit from "./FogDeposit";
 import FogWithdrawAll from "./FogWithdrawAll";
 import FogWithdrawPart from "./FogWithdrawPart";
 import FogFee from "./FogFee";
-import FogAllowance from "./FogAllowance";
 
 function Fog() {
   return (
     <div>
       <FogBalances />
       <FogApprove />
-      <FogAllowance/>
       <FogFee />
       <FogHash />
       <FogDeposit />
--- src/components/MixerOps/FogBalances.js
+++ src/components/MixerOps/FogBalances.js
@@ -1,23 +1,52 @@
 import React from 'react';
 import { useState } from 'react';
 import { useAccount } from '../../Store';  
+import { AddressCoinfog } from "../AddressABI/AddressCoinfog";
 
 function FogBalances() {
 
+  const {ethereum} = window;
+
   const contractCoinFog = useAccount(state => state.contractCoinfog2);
+  const contractTokenA = useAccount(state => state.contractTokenA2);
 
   let [displayStatus, setDisplayStatus] = useState(false);
   let [balanceTokenA, setBalanceTokenA] = useState("");
   let [txFee, setTxFee] = useState("");
-
+  let [userAllowanceAmount, setUserAllowanceAmount] = useState("");
 
   const getBalances = async () => {
+    
     setDisplayStatus(!displayStatus);
+
+    //if display status is false, we will save website from executing all code below
+    if(displayStatus === false) {
+      return;
+    }
+
+    //getting user address who calls this function
+    let userAccount;
+    if(window.ethereum !== "undefined") {
+        const accounts = await ethereum.request({method: "eth_requestAccounts"});
+        userAccount = accounts[0];
+    } else {
+        alert("Please install Metamask");
+        return;
+    }
+
+            
+    //checking userallowance
+    let userAllowance = await contractTokenA.allowance(userAccount, AddressCoinfog);
+    let userAllowance2 = userAllowance / (10**18);
+    let userAllowance3 = userAllowance2.toString();
+
+    //getting fee data
     let fee = await contractCoinFog.fee();
     let fee2 = fee / (10**18);
     let tokaBalance = await contractCoinFog.getContractTokenBalance();
     setBalanceTokenA(tokaBalance.toString())
     setTxFee(fee2.toString());
+    setUserAllowanceAmount(userAllowance3);
   }
 
   return (
@@ -27,6 +56,7 @@ function FogBalances() {
         displayStatus ?
         <>
           <strong><span>CoinFog TokenA Pool Balance:</span></strong> {balanceTokenA} TokenA<br />
+          <strong>User Allowance:</strong> {userAllowanceAmount} TokenA <br />
           <strong>Deposit and Withdrawal Fee:</strong> {txFee} FTM <br /> <br />
         </>
         :
